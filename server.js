6fcf034599c248065ba17b0fabb2d845e73a1911
EduYube <eduyuberol@gmail.com>
2025-12-06 17:23:31 +0100

    first tour created on hosted database

--- server.js
+++ server.js
@@ -14,6 +14,33 @@ mongoose
     useFindAndModify: false
   }).then(() => console.log('DB connection successful!'));
 
+const tourSchema = new mongoose.Schema({
+  name: {
+    type: String,
+    required: [true, 'A tour must have a name'],
+    unique: true,
+  },
+  price: {
+    type: Number,
+    required: [true, 'A tour must have a price'],
+  },
+  rating: Number,
+})
+
+const Tour = mongoose.model('Tour', tourSchema);
+
+const testTour = new Tour({
+  name: 'The Forest Hiker',
+  price: 497,
+  rating: 4.7,
+});
+
+testTour.save().then(doc => {
+  console.log('Tour saved:', doc);
+}).catch(err => {
+  console.error('Error saving tour:', err);
+});
+
 // Start server region
 app.listen(port, () => {
   console.log(`Server is running on http://localhost:${port}`);
