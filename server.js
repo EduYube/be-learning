1db4f682f883da489b0d982e37e1752f7eab8759
EduYube <eduyuberol@gmail.com>
2025-12-06 17:05:46 +0100

    create mogno connection

--- server.js
+++ server.js
@@ -3,6 +3,17 @@ dotenv.config({ path: './config.env' });
 
 const app = require('./app');
 const port = process.env.PORT || 3000;
+
+const mongoose = require('mongoose');
+const DB = process.env.DATABASE.replace('<PASSWORD>', process.env.DATABASE_PASSWORD);
+
+mongoose
+  .connect(DB, {
+    useNewUrlParser: true,  
+    useCreateIndex: true,
+    useFindAndModify: false
+  }).then(() => console.log('DB connection successful!'));
+
 // Start server region
 app.listen(port, () => {
   console.log(`Server is running on http://localhost:${port}`);
